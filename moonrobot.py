0ab102f95d96df31c7135e66a309986bf8c0759a
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-04 23:44:01 +0300

    moonrobot_notion_token

--- moonrobot.py
+++ moonrobot.py
@@ -3,6 +3,7 @@ import os
 from telegram.ext import Updater, CommandHandler
 
 MOONROBOT_TELEGRAM_TOKEN = os.environ['MOONROBOT_TELEGRAM_TOKEN']
+MOONROBOT_NOTION_TOKEN = os.environ['MOONROBOT_NOTION_TOKEN']
 
 updater = Updater(token=MOONROBOT_TELEGRAM_TOKEN)
 dispatcher = updater.dispatcher
@@ -12,8 +13,7 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+dispatcher.add_handler(CommandHandler('start', start))
 
 if __name__ == '__main__':
     updater.start_polling()
