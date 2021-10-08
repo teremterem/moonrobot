9700337b029efe078555bb98bee89253fb9ad409
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-08 21:01:43 +0300

    code that I don't need (yet)

--- botapp/views.py
+++ botapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
+# import json
+#
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+#
+#
+# @csrf_exempt
+# def telegram_webhook(request):
+#     # logger.info(f'{str(request.body)} <=== {type(request.body)}')
+#     body_json = json.loads(request.body)
+#     # logger.info(f'{str(body_json)} <=== {type(body_json)}')
+#     dispatch_telegram_update(body_json)
+#     return HttpResponse(status=200)
+#
+#
+# # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#custom-solution
+# def dispatch_telegram_update(json_request):
+#     plur.plur_bot.update_queue.put(
+#         Update.de_json(json_request, plur.plur_bot.bot)
+#     )
+#     # XXX do we need to suppress exceptions with try/catch here ?
