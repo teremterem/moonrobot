32fb2aa827333b13d04dbb2c88ac35ebe2a4608d
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-10 18:40:57 +0300

    in progress: reorganize modules

--- moonrobot_django/urls.py
+++ moonrobot_django/urls.py
@@ -1,4 +1,4 @@
-"""moonrobot URL Configuration
+"""moonrobot_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- moonrobot_django/wsgi.py
+++ moonrobot_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for moonrobot project.
+WSGI config for moonrobot_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moonrobot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moonrobot_django.settings')
 
 application = get_wsgi_application()
