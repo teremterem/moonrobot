b36c593c1e1c441ac86018ff670cd7989e75a195
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-10 16:38:05 +0300

    in progress: store messages in DB

--- botapp/models.py
+++ botapp/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 
-class MrbUser(models.Model):
+class NotionSynchable(models.Model):
+    class Meta:
+        abstract = True
+
+
+class MrbUser(NotionSynchable):
     pass
 
 
-class MrbChat(models.Model):
+class MrbChat(NotionSynchable):
     pass
 
 
-class MrbMessage(models.Model):
+class MrbMessage(NotionSynchable):
     # unique_msg_id = models.CharField(max_length=63, unique=True)
     pass
 
