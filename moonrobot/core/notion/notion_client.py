b5831d7730402c7b75bd449f295874ed022516b4
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-14 14:47:03 +0300

    notion_rich_text.py

--- moonrobot/core/notion/notion_client.py
+++ moonrobot/core/notion/notion_client.py
@@ -1,12 +1,12 @@
-import html
 import logging
 from pprint import pformat
-from typing import Collection
 
 import requests
 from django.conf import settings
 # noinspection PyPackageRequirements
 from telegram.utils.types import JSONDict
+
+from moonrobot.core.notion.notion_rich_text import collect_plain_text, collect_html_text
 
 logger = logging.getLogger(__name__)
 
@@ -35,37 +35,6 @@ def query_notion_db(database_id: str) -> JSONDict:
 
 def create_notion_page(body: JSONDict) -> JSONDict:
     return request_notion('pages', body_json=body)
-
-
-def collect_plain_text(rich_text_list: Collection[JSONDict]) -> str:
-    text = ''.join([i['plain_text'] for i in rich_text_list])
-    return text
-
-
-def collect_html_text(rich_text_list: Collection[JSONDict]) -> str:
-    def decorate_piece(piece):
-        piece_text = html.escape(piece['text']['content'])
-        piece_annotations = piece['annotations']
-
-        if piece_annotations['code']:
-            piece_text = f"<code>{piece_text}</code>"
-        if piece_annotations['bold']:
-            piece_text = f"<b>{piece_text}</b>"
-        if piece_annotations['italic']:
-            piece_text = f"<i>{piece_text}</i>"
-        if piece_annotations['strikethrough']:
-            piece_text = f"<s>{piece_text}</s>"
-        if piece_annotations['underline']:
-            piece_text = f"<u>{piece_text}</u>"
-
-        piece_link = (piece['text']['link'] or {}).get('url')
-        if piece_link:
-            piece_text = f"<a href=\"{piece_link}\">{piece_text}</a>"
-
-        return piece_text
-
-    text = ''.join([decorate_piece(i) for i in rich_text_list])
-    return text
 
 
 def fetch_entrypoint_dict() -> JSONDict:
---
+++ moonrobot/core/notion/notion_rich_text.py
@@ -0,0 +1,36 @@
+import html
+from typing import Collection
+
+# noinspection PyPackageRequirements
+from telegram.utils.types import JSONDict
+
+
+def collect_plain_text(rich_text_list: Collection[JSONDict]) -> str:
+    text = ''.join([i['plain_text'] for i in rich_text_list])
+    return text
+
+
+def collect_html_text(rich_text_list: Collection[JSONDict]) -> str:
+    def decorate_piece(piece):
+        piece_text = html.escape(piece['text']['content'])
+        piece_annotations = piece['annotations']
+
+        if piece_annotations['code']:
+            piece_text = f"<code>{piece_text}</code>"
+        if piece_annotations['bold']:
+            piece_text = f"<b>{piece_text}</b>"
+        if piece_annotations['italic']:
+            piece_text = f"<i>{piece_text}</i>"
+        if piece_annotations['strikethrough']:
+            piece_text = f"<s>{piece_text}</s>"
+        if piece_annotations['underline']:
+            piece_text = f"<u>{piece_text}</u>"
+
+        piece_link = (piece['text']['link'] or {}).get('url')
+        if piece_link:
+            piece_text = f"<a href=\"{piece_link}\">{piece_text}</a>"
+
+        return piece_text
+
+    text = ''.join([decorate_piece(i) for i in rich_text_list])
+    return text
