948f6d7b00826354bac5a1617cf9994d77b24350
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-16 21:54:19 +0300

    bot replies to messages

--- moonrobot/admin.py
+++ moonrobot/admin.py
@@ -3,22 +3,36 @@ from django.contrib.admin import ModelAdmin
 from django.db.models import QuerySet
 from django.http import HttpRequest
 # noinspection PyPackageRequirements
-from telegram import Update
+from telegram import Update, Message
 
 from moonrobot.core.telegram_bot import get_bot
 from moonrobot.models import MrbBot, MrbUser, MrbChat, MrbMessage, MrbUserMessage, MrbBotMessage
 
 
+def extract_telegram_message(mrb_msg: MrbMessage) -> Message:
+    message = None
+    if isinstance(mrb_msg, MrbUserMessage):
+        update = Update.de_json(mrb_msg.update_payload, get_bot())
+        message = update.effective_message
+    elif isinstance(mrb_msg, MrbBotMessage):
+        message = Message.de_json(mrb_msg.response_payload, get_bot())
+    return message
+
+
 # noinspection PyUnusedLocal
 @admin.action(description='Reply')
 def reply(modeladmin: 'MrbUserMessageAdmin', request: HttpRequest, queryset: QuerySet) -> None:
-    msg: MrbUserMessage
-    for msg in queryset:
-        update = Update.de_json(msg.update_payload, get_bot())
-        update.effective_message.reply_text('hello taporld')
+    mrb_msg: MrbUserMessage
+    for mrb_msg in queryset:
+        message = extract_telegram_message(mrb_msg)
+        message.reply_text('hello taporld')
 
 
 class MrbUserMessageAdmin(ModelAdmin):
+    actions = [reply]
+
+
+class MrbBotMessageAdmin(ModelAdmin):
     actions = [reply]
 
 
@@ -27,4 +41,4 @@ admin.site.register(MrbUser)
 admin.site.register(MrbChat)
 admin.site.register(MrbMessage)
 admin.site.register(MrbUserMessage, MrbUserMessageAdmin)
-admin.site.register(MrbBotMessage)
+admin.site.register(MrbBotMessage, MrbBotMessageAdmin)
