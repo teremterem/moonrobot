e215e4a98d4f1b58379deef7acfbb08e89126d24
Oleksandr Tereshchenko <toporok@gmail.com>
2021-10-17 12:50:26 +0300

    rename moonrobot_django to django_moonrobot

--- django_moonrobot/asgi.py
+++ django_moonrobot/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for moonrobot_django project.
+ASGI config for django_moonrobot project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moonrobot_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_moonrobot.settings')
 
 application = get_asgi_application()
--- django_moonrobot/wsgi.py
+++ django_moonrobot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for moonrobot_django project.
+WSGI config for django_moonrobot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moonrobot_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_moonrobot.settings')
 
 application = get_wsgi_application()
